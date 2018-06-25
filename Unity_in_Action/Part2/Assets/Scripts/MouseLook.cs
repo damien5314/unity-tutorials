6a140a149817b4e4a93aa2e2d3adedd5545d8342
Damien Diehl <damien5314@gmail.com>
2018-06-25 12:08:11 +0900

    Add MouseLook and PlayerMovement scripts

--- Unity_in_Action/Part2/Assets/Scripts/MouseLook.cs
+++ Unity_in_Action/Part2/Assets/Scripts/MouseLook.cs
@@ -1,16 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class MouseLook : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class MouseLook : MonoBehaviour
+{
 	
-	// Update is called once per frame
-	void Update () {
-		
+	public float VerticalSensitivity = 6.0f;
+	public float MinRotation = -90.0f;
+	public float MaxRotation = 90.0f;
+
+	private float _rotation = 0;
+
+	void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+
+	void Update()
+	{
+		_rotation -= Input.GetAxis("Mouse Y") * VerticalSensitivity;
+		// Prevent camera from flipping upside down
+		_rotation = Mathf.Clamp(_rotation, MinRotation, MaxRotation);
+		transform.localEulerAngles = new Vector3(_rotation, transform.localEulerAngles.y, 0);
 	}
 }
--- Unity_in_Action/Part2/Assets/Scripts/PlayerMovement.cs
+++ Unity_in_Action/Part2/Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public float HorizontalSensitivity = 6.0f;
+	public float MovementSpeed = 1.0f;
+
+	private CharacterController _characterController;
+
+	void Start()
+	{
+		_characterController = GetComponent<CharacterController>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update()
+	{
+		UpdateRotation();
+		UpdateMovement();
+	}
+
+	void UpdateRotation()
+	{
+		float xDir = Input.GetAxis("Mouse X");
+		transform.Rotate(0, xDir * HorizontalSensitivity, 0, Space.Self);
+	}
+
+	void UpdateMovement()
+	{
+		float xMove = Input.GetAxis("Horizontal") * MovementSpeed;
+		float zMove = Input.GetAxis("Vertical") * MovementSpeed;
+
+		Vector3 movement = Vector3.ClampMagnitude(new Vector3(xMove, 0, zMove), MovementSpeed);
+		movement *= Time.deltaTime;
+		movement = transform.TransformDirection(movement);
+
+		_characterController.Move(movement);
 	}
 }
