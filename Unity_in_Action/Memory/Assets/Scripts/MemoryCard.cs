378a87c6781da80efbfffa9faa561043d35b86fe
Damien Diehl <damien5314@gmail.com>
2018-06-28 13:15:45 +0900

    Add code to flip Memory cards

---
+++ Unity_in_Action/Memory/Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+
+	[SerializeField] private GameObject _cardBack;
+	
+	private void OnMouseDown()
+	{
+		// What's the difference between active and activeSelf?
+		if (_cardBack.activeSelf)
+		{
+			_cardBack.SetActive(false);
+		}
+	}
+}
