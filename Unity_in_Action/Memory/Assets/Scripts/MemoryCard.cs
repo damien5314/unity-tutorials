7a3531faa1dd4a575bae26fb0645a6f5c2ef7657
Damien Diehl <damien5314@gmail.com>
2018-06-28 14:01:50 +0900

    Add code to randomize the card displayed

--- Unity_in_Action/Memory/Assets/Scripts/MemoryCard.cs
+++ Unity_in_Action/Memory/Assets/Scripts/MemoryCard.cs
@@ -1,12 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MemoryCard : MonoBehaviour
 {
 
 	[SerializeField] private GameObject _cardBack;
-	
+	[SerializeField] private SceneController _sceneController;
+	private int _id;
+
+	public int Id
+	{
+		get { return _id; }
+		private set { _id = value; }
+	}
+
+	public void SetCard(int id, Sprite image)
+	{
+		Id = id;
+		GetComponent<SpriteRenderer>().sprite = image;
+	}
+
 	private void OnMouseDown()
 	{
 		// What's the difference between active and activeSelf?
---
+++ Unity_in_Action/Memory/Assets/Scripts/SceneController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+
+	[SerializeField] private MemoryCard _originalCard;
+	[SerializeField] private Sprite[] _images;
+
+	void Start()
+	{
+		int id = Random.Range(0, _images.Length);
+		_originalCard.SetCard(id, _images[id]);
+	}
+}
