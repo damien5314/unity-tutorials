2336f1f91d61df85386dbda700ca7f905ea88169
Damien Diehl <damien5314@gmail.com>
2018-06-28 22:28:14 +0900

    Add restart button

--- Unity_in_Action/Memory/Assets/Scripts/SceneController.cs
+++ Unity_in_Action/Memory/Assets/Scripts/SceneController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
@@ -112,5 +113,10 @@ public class SceneController : MonoBehaviour
 	{
 		_score = score;
 		_scoreLabel.text = "スコア: " + score;
+	}
+
+	public void Restart()
+	{
+		SceneManager.LoadScene("Main");
 	}
 }
