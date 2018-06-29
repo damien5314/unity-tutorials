a3a6a2bf2a0aae0a946438b0625ecab5d8c9dbea
Damien Diehl <damien5314@gmail.com>
2018-06-29 18:44:58 +0900

    Add basic UI controller script

---
+++ Unity_in_Action/3D_Demo_1/Assets/Scripts/UiController.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiController : MonoBehaviour
+{
+
+	[SerializeField] private Text _scoreLabel;
+	
+	private void Update()
+	{
+		_scoreLabel.text = Time.realtimeSinceStartup.ToString(CultureInfo.CurrentCulture);
+	}
+
+	public void OnOpenSettings()
+	{
+		Debug.Log("Opened settings");
+	}
+}
