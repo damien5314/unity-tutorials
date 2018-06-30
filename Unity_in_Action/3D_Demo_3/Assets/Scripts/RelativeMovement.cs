40a75d4e7838176d3dbede9030ce5e082d2852bb
Damien Diehl <damien5314@gmail.com>
2018-07-01 08:58:30 +0900

    Add script for smoothly rotating the camera

---
+++ Unity_in_Action/3D_Demo_3/Assets/Scripts/RelativeMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelativeMovement : MonoBehaviour
+{
+
+	[SerializeField] private Transform _target; // Reference to the camera
+	public float RotationSpeed = 15.0f;
+
+	private void Update()
+	{
+		Vector3 movement = Vector3.zero;
+
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+
+		if (horizontalInput != 0 || verticalInput != 0)
+		{
+			movement.x = horizontalInput;
+			movement.z = verticalInput;
+
+			Quaternion temp = _target.rotation;
+			_target.eulerAngles = new Vector3(0, _target.eulerAngles.y, 0);
+			movement = _target.TransformDirection(movement);
+			_target.rotation = temp;
+
+			Quaternion direction = Quaternion.LookRotation(movement);
+			transform.rotation = Quaternion.Lerp(transform.rotation, direction, RotationSpeed * Time.deltaTime);
+		}
+	}
+}
