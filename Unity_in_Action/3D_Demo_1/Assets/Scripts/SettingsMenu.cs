356960a0bcec8968393eeade3879e0b417b61ffe
Damien Diehl <damien5314@gmail.com>
2018-06-30 09:26:20 +0900

    Hook up logic to save settings

--- Unity_in_Action/3D_Demo_1/Assets/Scripts/SettingsMenu.cs
+++ Unity_in_Action/3D_Demo_1/Assets/Scripts/SettingsMenu.cs
@@ -1,8 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class SettingsMenu : MonoBehaviour {
+public class SettingsMenu : MonoBehaviour
+{
+
+	private const string PrefsName = "prefs_name";
+	private const string PrefsSpeed = "prefs_speed";
+
+	[SerializeField] private InputField _nameField;
+	[SerializeField] private Slider _speedSlider;
+
+	private void Start()
+	{
+		_nameField.text = PlayerPrefs.GetString(PrefsName);
+		_speedSlider.value = PlayerPrefs.GetFloat(PrefsSpeed);
+	}
 
 	public void Open()
 	{
@@ -12,5 +24,15 @@ public class SettingsMenu : MonoBehaviour {
 	public void Close()
 	{
 		gameObject.SetActive(false);
+	}
+
+	public void OnSubmitName(string submitted)
+	{
+		PlayerPrefs.SetString(PrefsName, submitted);
+	}
+
+	public void OnSpeedValue(float speed)
+	{
+		PlayerPrefs.SetFloat(PrefsSpeed, speed);
 	}
 }
