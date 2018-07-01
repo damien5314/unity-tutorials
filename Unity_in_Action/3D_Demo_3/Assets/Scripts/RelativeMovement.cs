e488c3f6c5308ad0593aa302a1ed0f18c022c7cb
Damien Diehl <damien5314@gmail.com>
2018-07-01 17:56:23 +0900

    Add code for jumping + platforms in scene

--- Unity_in_Action/3D_Demo_3/Assets/Scripts/RelativeMovement.cs
+++ Unity_in_Action/3D_Demo_3/Assets/Scripts/RelativeMovement.cs
@@ -9,12 +9,18 @@ public class RelativeMovement : MonoBehaviour
 	[SerializeField] private Transform _target; // Reference to the camera
 	public float RotationSpeed = 15.0f;
 	public float MovementSpeed = 6.0f;
+	public float JumpSpeed = 15.0f;
+	public float Gravity = -9.8f;
+	public float TerminalVelocity = -10.0f;
+	public float MinimumFall = -1.5f;
 
 	private CharacterController _characterController;
+	private float _verticalSpeed;
 
 	private void Start()
 	{
 		_characterController = GetComponent<CharacterController>();
+		_verticalSpeed = MinimumFall;
 	}
 
 	private void Update()
@@ -24,6 +30,7 @@ public class RelativeMovement : MonoBehaviour
 		float horizontalInput = Input.GetAxis("Horizontal");
 		float verticalInput = Input.GetAxis("Vertical");
 
+		// Calculate movement in the X and Z directions based on player input
 		if (horizontalInput != 0 || verticalInput != 0)
 		{
 			movement.x = horizontalInput * MovementSpeed;
@@ -38,6 +45,30 @@ public class RelativeMovement : MonoBehaviour
 			Quaternion direction = Quaternion.LookRotation(movement);
 			transform.rotation = Quaternion.Lerp(transform.rotation, direction, RotationSpeed * Time.deltaTime);
 		}
+		
+		// Calculate movement in the Y direction based on gravity
+		if (_characterController.isGrounded)
+		{
+			if (Input.GetButtonDown("Jump"))
+			{
+				_verticalSpeed = JumpSpeed;
+			}
+			else
+			{
+				_verticalSpeed = MinimumFall;
+			}
+		}
+		else
+		{
+			// TODO: Should we actually be double-applying Time.deltaTime here?
+			_verticalSpeed += Gravity * 5 * Time.deltaTime;
+			if (_verticalSpeed < TerminalVelocity)
+			{
+				_verticalSpeed = TerminalVelocity;
+			}
+		}
+
+		movement.y = _verticalSpeed;
 
 		movement *= Time.deltaTime;
 		_characterController.Move(movement);
