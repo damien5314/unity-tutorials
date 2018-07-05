b6eab815322ee9bfba9992d196a9c13d192e6499
Damien Diehl <damien5314@gmail.com>
2018-07-05 15:51:49 +0900

    Add code for the game managers

---
+++ Unity_in_Action/3D_Demo_3/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class InventoryManager : MonoBehaviour, IGameManager
+{
+	public ManagerStatus status { get; private set; }
+	
+	public void Startup()
+	{
+		Debug.Log("InventoryManager started...");
+		status = ManagerStatus.Started;
+	}
+}
---
+++ Unity_in_Action/3D_Demo_3/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerManager : MonoBehaviour, IGameManager
+{
+	public ManagerStatus status { get; private set; }
+
+	public int Health;
+	public int MaxHealth;
+	
+	public void Startup()
+	{
+		Debug.Log("PlayerManager starting...");
+
+		Health = 50;
+		MaxHealth = 100;
+		
+		status = ManagerStatus.Started;
+	}
+
+	public void ChangeHealth(int value)
+	{
+		Health = value;
+
+		if (value > MaxHealth)
+		{
+			Health = MaxHealth;
+		} 
+		else if (value < 0)
+		{
+			Health = 0;
+		}
+		
+		Debug.Log(string.Format("Health: {0} / {1}", Health, MaxHealth));
+	}
+}
