d6dfd085736f89acb5e20fd415e2eb0d3e9efdeb
Damien Diehl <damien5314@gmail.com>
2018-07-05 11:30:08 +0900

    Add script to change button colors randomly

---
+++ Unity_in_Action/3D_Demo_3/Assets/Scripts/ColorChangeDevice.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeDevice : MonoBehaviour {
+
+	public void Operate()
+	{
+		Color random = new Color(
+			r: Random.Range(0f, 1f),
+			g: Random.Range(0f, 1f),
+			b: Random.Range(0f, 1f)
+		);
+
+		GetComponent<Renderer>().material.color = random;
+	}
+}
