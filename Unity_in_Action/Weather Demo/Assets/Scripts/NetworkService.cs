64216f220025f2451a3cfac8f8ba8925c1397889
Damien Diehl <damien5314@gmail.com>
2018-07-06 17:48:54 +0900

    Refactor NetworkService code into WeatherManager

--- Unity_in_Action/Weather Demo/Assets/Scripts/NetworkService.cs
+++ Unity_in_Action/Weather Demo/Assets/Scripts/NetworkService.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class NetworkService
 {
 
-	public const string WeatherEndpoint =
-		"https://api.openweathermap.org/data/2.5/weather?q=Chicago,us&appid=c54203e013cb940efbc7d5a2fcc46b1b";
-
 	private bool IsResponseValid(WWW www)
 	{
 		if (www.error != null)
@@ -25,7 +22,7 @@ public class NetworkService
 		return true;
 	}
 
-	private IEnumerator CallApi(string url, Action<string> callback)
+	public IEnumerator CallApi(string url, Action<string> callback)
 	{
 		WWW www = new WWW(url);
 		yield return www;
@@ -33,10 +30,5 @@ public class NetworkService
 		if (!IsResponseValid(www)) yield break;
 
 		callback(www.text);
-	}
-
-	public IEnumerator GetWeatherXml(Action<string> callback)
-	{
-		return CallApi(WeatherEndpoint, callback);
 	}
 }
--- Unity_in_Action/Weather Demo/Assets/Scripts/WeatherManager.cs
+++ Unity_in_Action/Weather Demo/Assets/Scripts/WeatherManager.cs
@@ -1,6 +1,10 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class WeatherManager : MonoBehaviour, IGameManager {
+
+	public const string WeatherEndpoint =
+		"https://api.openweathermap.org/data/2.5/weather?q=Chicago,us&appid=c54203e013cb940efbc7d5a2fcc46b1b";
 	
 	public ManagerStatus Status { get; private set; }
 	
@@ -12,12 +16,17 @@ public class WeatherManager : MonoBehaviour, IGameManager {
 		Debug.Log("Weather manager starting...");
 		
 		_network = service;
-		StartCoroutine(_network.GetWeatherXml(OnXmlDataLoaded));
+		StartCoroutine(GetWeatherXml());
 
 		Status = ManagerStatus.Initializing;
 	}
 
-	public void OnXmlDataLoaded(string data)
+	private IEnumerator GetWeatherXml()
+	{
+		return _network.CallApi(WeatherEndpoint, OnXmlDataLoaded);
+	}
+
+	private void OnXmlDataLoaded(string data)
 	{
 		Debug.Log(data);
 
