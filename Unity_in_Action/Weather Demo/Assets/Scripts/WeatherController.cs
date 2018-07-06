8a1601aa3c728580d49e59b0b77feb25a9cd4f2c
Damien Diehl <damien5314@gmail.com>
2018-07-06 13:41:20 +0900

    Add logic to auto-transition the skybox shader

---
+++ Unity_in_Action/Weather Demo/Assets/Scripts/WeatherController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class WeatherController : MonoBehaviour
+{
+	
+	private const float CloudIncrement = 0.005f;
+
+	[SerializeField] private Material _sky;
+	[SerializeField] private Light _sun;
+
+	private float _fullIntensity;
+	private float _cloudValue = 0f;
+
+	private void Start()
+	{
+		_fullIntensity = _sun.intensity;
+	}
+
+	private void Update()
+	{
+		SetOvercast(_cloudValue);
+		_cloudValue += CloudIncrement;
+	}
+
+	private void SetOvercast(float value)
+	{
+//		_sky.SetFloat("_Blend", value);
+		_sky.SetFloat("_Blend", Math.Min(1, value));
+		_sun.intensity = _fullIntensity - (_fullIntensity * value);
+	}
+}
