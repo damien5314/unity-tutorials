a3e6ef230e8103f748c6b506e0fc44eaf639cb79
Damien Diehl <damien5314@gmail.com>
2018-07-06 14:34:44 +0900

    Add GameManagers + WeatherManager

---
+++ Unity_in_Action/Weather Demo/Assets/Scripts/WeatherManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class WeatherManager : MonoBehaviour, IGameManager {
+	
+	public ManagerStatus Status { get; private set; }
+	
+	// Add cloud value here (listing 9.8)
+	private NetworkService _network;
+	
+	public void Startup(NetworkService service)
+	{
+		Debug.Log("Weather manager starting...");
+		
+		_network = service;
+
+		Status = ManagerStatus.Started;
+	}
+}
