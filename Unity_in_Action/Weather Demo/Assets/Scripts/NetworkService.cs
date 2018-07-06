41604cd160ddbdf99bd9114e3cdf087c5d735997
Damien Diehl <damien5314@gmail.com>
2018-07-06 15:12:32 +0900

    Add networking code for requesting weather data

--- Unity_in_Action/Weather Demo/Assets/Scripts/NetworkService.cs
+++ Unity_in_Action/Weather Demo/Assets/Scripts/NetworkService.cs
@@ -1,6 +1,42 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
-public class NetworkService {
-	
-	
+public class NetworkService
+{
+
+	public const string WeatherEndpoint =
+		"https://api.openweathermap.org/data/2.5/weather?q=Chicago,us&appid=c54203e013cb940efbc7d5a2fcc46b1b";
+
+	private bool IsResponseValid(WWW www)
+	{
+		if (www.error != null)
+		{
+			Debug.Log("Bad connection");
+			return false;
+		}
+
+		if (string.IsNullOrEmpty(www.text))
+		{
+			Debug.Log("Bad data");
+			return false;
+		}
+
+		return true;
+	}
+
+	private IEnumerator CallApi(string url, Action<string> callback)
+	{
+		WWW www = new WWW(url);
+		yield return www;
+
+		if (!IsResponseValid(www)) yield break;
+
+		callback(www.text);
+	}
+
+	public IEnumerator GetWeatherXml(Action<string> callback)
+	{
+		return CallApi(WeatherEndpoint, callback);
+	}
 }
--- Unity_in_Action/Weather Demo/Assets/Scripts/WeatherManager.cs
+++ Unity_in_Action/Weather Demo/Assets/Scripts/WeatherManager.cs
@@ -12,6 +12,14 @@ public class WeatherManager : MonoBehaviour, IGameManager {
 		Debug.Log("Weather manager starting...");
 		
 		_network = service;
+		StartCoroutine(_network.GetWeatherXml(OnXmlDataLoaded));
+
+		Status = ManagerStatus.Initializing;
+	}
+
+	public void OnXmlDataLoaded(string data)
+	{
+		Debug.Log(data);
 
 		Status = ManagerStatus.Started;
 	}
