a1779d5352a67f88bf98bfaed92b75b76ea30500
Damien Diehl <damien5314@gmail.com>
2018-08-06 23:43:40 -0700

    Use PlayerManager health instead of PlayerCharacter

--- Unity_in_Action/Action_RPG/Assets/Scripts/PlayerCharacter.cs
+++ Unity_in_Action/Action_RPG/Assets/Scripts/PlayerCharacter.cs
@@ -2,18 +2,9 @@
 
 public class PlayerCharacter : MonoBehaviour
 {
-	public int Health = 5;
-
-	private int _remainingHealth;
-
-	void Start()
-	{
-		_remainingHealth = Health;
-	}
 
 	public void Hurt(int damage)
 	{
-		_remainingHealth -= damage;
-		Debug.Log("Health: " + _remainingHealth);
+		Managers.Player.ChangeHealth(damage * -1);
 	}
 }
