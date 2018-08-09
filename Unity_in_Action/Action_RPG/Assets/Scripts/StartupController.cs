c2f492f3d932715aa5654241ae06d7461456c04f
Damien Diehl <damien5314@gmail.com>
2018-08-08 23:40:17 -0700

    Split out the Startup scene

---
+++ Unity_in_Action/Action_RPG/Assets/Scripts/StartupController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StartupController : MonoBehaviour
+{
+
+	[SerializeField] private Slider _progressBar;
+
+	private void Awake()
+	{
+		Messenger<int, int>.AddListener(StartupEvent.MANAGERS_PROGRESS, OnManagersProgress);
+		Messenger.AddListener(StartupEvent.MANAGERS_STARTED, OnManagersStarted);
+	}
+
+	private void OnDestroy()
+	{
+		Messenger<int, int>.RemoveListener(StartupEvent.MANAGERS_PROGRESS, OnManagersProgress);
+		Messenger.RemoveListener(StartupEvent.MANAGERS_STARTED, OnManagersStarted);
+	}
+
+	private void OnManagersProgress(int numReady, int numModules)
+	{
+		float progress = (float) numReady / numModules;
+		_progressBar.value = progress;
+	}
+
+	private void OnManagersStarted()
+	{
+		Managers.Mission.GoToNext();
+	}
+}
