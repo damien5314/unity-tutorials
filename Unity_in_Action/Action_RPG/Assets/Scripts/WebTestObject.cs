d28e033d6453b59e1083c08f975563cdbfceb3a4
Damien Diehl <damien5314@gmail.com>
2018-08-11 15:29:04 -0700

    Add communication between web page and Unity

---
+++ Unity_in_Action/Action_RPG/Assets/Scripts/WebTestObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class WebTestObject : MonoBehaviour
+{
+
+	private string _message;
+
+	private void Start()
+	{
+		_message = "No message yet";
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Application.ExternalCall("ShowAlert", "Hello out there!");
+		}
+	}
+
+	private void OnGUI()
+	{
+		GUI.Label(new Rect(10, 10, 200, 20), _message);
+	}
+
+	public void RespondToBrowser(string message)
+	{
+		_message = message;
+	}
+}
